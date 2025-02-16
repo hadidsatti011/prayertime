f073cf047f4f5bde160689e1b66b1f65ed4ce051
hadidsatti011 <numl-f23-46115@numls.edu.pk>
2025-02-16 19:23:17 +0500

    Updated manifest.json and service worker

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,7 @@
-const CACHE_NAME = 'prayer-times-cache-v1';
-const urlsToCache = ['/', '/index.html', '/styles.css', '/app.js'];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-    );
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => response || fetch(event.request))
-    );
-});
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(reg => console.log('Service Worker registered!', reg))
+            .catch(err => console.error('Service Worker registration failed:', err));
+    });
+}
